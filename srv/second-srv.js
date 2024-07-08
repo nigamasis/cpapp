5a1c8e3990e40ca461c5dd4154fc1714627694e4
Nigamasis Padhi <nnpadhi66@gmail.com>
2024-07-08 18:35:49 +0000

    Setting up for sender profile

--- srv/second-srv.js
+++ srv/second-srv.js
@@ -2,12 +2,12 @@ const cds = require("@sap/cds");
 
 module.exports = cds.service.impl(async function () {
 
-    const db = await cds.connect.to ('db');
-    const { User } = db.entities ('sap.sample.cpapp');
+    // const db = await cds.connect.to ('db');
+    // const { User } = db.entities ('sap.sample.cpapp');
 
-    const incSrv = await cds.connect.to ('incidentService');
-    incSrv.on('addUser', async(msg) => {
-        return incSrv.run(INSERT.into(User).entries({userID: msg.data.uid, name: msg.data.fname, gender: msg.data.gender, prodID_ProductID: msg.data.prodID}));
-    });
+    // const incSrv = await cds.connect.to ('incidentService');
+    // incSrv.on('addUser', async(msg) => {
+    //     return incSrv.run(INSERT.into(User).entries({userID: msg.data.uid, name: msg.data.fname, gender: msg.data.gender, prodID_ProductID: msg.data.prodID}));
+    // });
 
 });
